c5184b08f97b094506a65c34fbdfe7676054f281
YuanYuan <yyuan201@gmail.com>
2016-03-29 22:11:30 +0800

    remove database and update functional test

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -7,19 +7,23 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+
     @classmethod
     def setUpClass(cls):
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
+                cls.use_own_url = True
                 return
-            super().setUpClass()
-            cls.server_url = cls.live_server_url
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+        cls.use_own_url = False
 
     @classmethod
     def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
+        if not cls.use_own_url:
             super().tearDownClass()
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
